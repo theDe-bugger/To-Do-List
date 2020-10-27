9124bc8d7d29835d429793312a69cb89f3118750
Jaimil Dalwadi <39176231+theDe-bugger@users.noreply.github.com>
2020-10-27 01:03:58 +0000

    fixes

--- todolist.js
+++ todolist.js
@@ -1,27 +1,26 @@
 var list = document.getElementById('list');
-
+var i = 0;
 function add(){
-    i = 0;
     var list = document.getElementById('list');
     var goal = document.createTextNode(document.getElementById('goal').value);
     var checkbox = document.createElement("input");
         checkbox.setAttribute("type","checkbox");
-        checkbox.setAttribute("onclick","remove()");
         checkbox.setAttribute("name","check");
+        checkbox.setAttribute("onclick","remove()");
     var li = document.createElement('LI');
     li.id = "item" + String(i)
     li.appendChild(checkbox);
     li.appendChild(goal);
     list.appendChild(li);
+    i++;
 }
 
-function remove(){
+function remove(e){
     var list = document.getElementById("list");
     var checked = document.getElementsByName("check");
     for(i=0;i<checked.length;i++){
         if(checked[i].checked==true){
-            setTimeout(() => {  list.removeChild(list.childNodes[i])}, 1000);
-            
+            list.removeChild(list.childNodes[i]);
         }
     }
 }
